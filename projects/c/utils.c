120ea873f516b5c896f1c9ed879c1026a6d3f5ea
bugcatching <142191434+bugcatching@users.noreply.github.com>
2023-09-10 01:41:27 +0200

    Update utils.c

--- projects/c/utils.c
+++ projects/c/utils.c
@@ -13,3 +13,32 @@ void stripPunctuation(char *s, char *o) {
 
   o[j] = '\0';
 }
+
+void heapify(int arr[], int N, int i) {
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+  if (left < N && arr[left] > arr[largest])
+
+    largest = left;
+
+  if (right < N && arr[right] > arr[largest])
+
+    largest = right;
+
+  if (largest != i) {
+
+    SWAP(&arr[i], &arr[largest], int);
+    heapify(arr, N, largest);
+  }
+}
+
+void heapSort(int arr[], int N) {
+  for (int i = N / 2 - 1; i >= 0; i--)
+    heapify(arr, N, i);
+
+  for (int i = N - 1; i >= 0; i--) {
+    SWAP(&arr[0], &arr[i], int);
+    heapify(arr, i, 0);
+  }
+}
